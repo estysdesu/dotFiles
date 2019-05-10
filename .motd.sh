3f8fcaf0fe1078c2d6af0868b440c737d00a064e
Tyler Estes <estysdesu@gmail.com>
2019-05-11 01:26:32 +0900

    add *.swp file to .gitignore

--- .motd.sh
+++ .motd.sh
@@ -3,4 +3,5 @@
 # large ascii with figlet; fortune piped to cowsay
 # figlet estysdesu
 # fortune | cowsay -f moose 
-printf "\nUser: %s\nHostname: %s\nDate : %s\nUptime : %s\n\n", "$(whoami)" "$(hostname -s)" "$(date)" "$(uptime)"
+printf "\nUser: %s\nHostname: %s\nDate : %s\nUptime : %s\n\n" "$(whoami)" "$(hostname -s)" "$(date)" "$(uptime)"
+
