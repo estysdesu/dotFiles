a5de45e9f0b6da3084efec895339792c755c1aef
Tyler Estes <estysdesu@gmail.com>
2019-06-09 21:14:57 +0900

    add tmux

--- symLinks.sh
+++ symLinks.sh
@@ -6,4 +6,5 @@ ln -sf $HOME/dotFiles/.gitignore $HOME/.gitignore
 ln -sf $HOME/dotFiles/.vimrc $HOME/.vimrc
 ln -sf $HOME/dotFiles/.Brewfile $HOME/.Brewfile
 ln -sf $HOME/dotFiles/motd.sh /etc/motd.sh
-ln -sf $HOME/dotFiles/neofetch/config.conf $HOME/.config/neofetch/config.conf
+ln -sf $HOME/dotFiles/.neofetch.conf $HOME/.config/neofetch/config.conf
+ln -sf $HOME/dotFiles/.tmux.conf $HOME/.tmux.conf
