ee7d40d172b7a5c246d21e63e2cefd3b8ec740d2
Tyler Estes <estysdesu@gmail.com>
2019-06-10 19:32:09 +0900

    update brewfile

--- symLinks.sh
+++ symLinks.sh
@@ -4,7 +4,7 @@ ln -sf $HOME/dotFiles/.bash_profile $HOME/.bash_profile
 ln -sf $HOME/dotFiles/.bashrc $HOME/.bashrc
 ln -sf $HOME/dotFiles/.gitignore $HOME/.gitignore
 ln -sf $HOME/dotFiles/.vimrc $HOME/.vimrc
-ln -sf $HOME/dotFiles/.Brewfile $HOME/.Brewfile
+ln -sf $HOME/dotFiles/.brewfile $HOME/.brewfile
 ln -sf $HOME/dotFiles/motd.sh /etc/motd.sh
 ln -sf $HOME/dotFiles/.neofetch.conf $HOME/.config/neofetch/config.conf
 ln -sf $HOME/dotFiles/.tmux.conf $HOME/.tmux.conf
