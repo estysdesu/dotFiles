2a6d2d786c353ab3bc649f77e76de35de17ae712
estysdesu <estsydesu@gmail.com>
2019-07-04 10:42:41 +0900

    update dotfiles from make

--- makefile
+++ makefile
@@ -1,18 +1,28 @@
-DOTFILES=${HOME}/dotFiles
+dotfiles=${HOME}/dotFiles
+brewfile=$(dotfiles)/homebrew/Brewfile
 
-.PHONY: brew-update git-update sym-refresh update
+.PHONY:
+	brew-update
+	brew-install
+	git-update
+	sym-refresh
+	update
 
 brew-update:
-	@echo "updating Brewfile"
-	brew bundle dump --file=$(DOTFILES)/Brewfile --force
+	@echo "updating Brewfile with all current Homebrew packages"
+	@brew bundle dump --file=$(brewfile) --force
+
+brew-install:
+	@echo "installing Homebrew packages in Brewfile"
+	@brew bundle --file=$(brewfile)
 
 git-update:
-	@echo "updating github repository"
-	git add . && git commit -m "update dotfiles from make" && git push
+	@echo "updating dotFiles github repository"
+	@git add . && git commit -m "update dotfiles from make" && git push
 
 sym-refresh:
 	@echo "refreshing all symlinks"
-	sh $(DOTFILES)/symLinks
+	@sh $(dotfiles)/symLinks
 
 update:
 	brew-update
--- sbin/macOSBootstrap.sh
+++ sbin/macOSBootstrap.sh
@@ -1,30 +1,20 @@
 #!/usr/bin/env bash
-#
+
 # Bootstrap script for setting up a new OSX machine
-#
 # This should be idempotent so it can be run multiple times.
-#
-# Some apps don't have a cask and so still need to be installed by hand. These
-# include:
-#
-# - Twitter (app store)
-# - Postgres.app (http://postgresapp.com/)
-#
 # Notes:
-#
-# - If installing full Xcode, it's better to install that first from the app
-#   store before running the bootstrap script. Otherwise, Homebrew can't access
-#   the Xcode libraries as the agreement hasn't been accepted yet.
-#
+#   - If installing full Xcode, it's better to install that first from the app
+#     store before running the bootstrap script. Otherwise, Homebrew can't access
+#     the Xcode libraries as the agreement hasn't been accepted yet.
 # Reading:
-#
-# - http://lapwinglabs.com/blog/hacker-guide-to-setting-up-your-mac
-# - https://gist.github.com/MatthewMueller/e22d9840f9ea2fee4716
-# - https://news.ycombinator.com/item?id=8402079
-# - http://notes.jerzygangi.com/the-best-pgp-tutorial-for-mac-os-x-ever/
+#   - http://lapwinglabs.com/blog/hacker-guide-to-setting-up-your-mac
+#   - https://gist.github.com/MatthewMueller/e22d9840f9ea2fee4716
+#   - https://news.ycombinator.com/item?id=8402079
+#   - http://notes.jerzygangi.com/the-best-pgp-tutorial-for-mac-os-x-ever/
 
 echo "Starting bootstrapping"
 
+##### HOMEBREW #####
 # Check for Homebrew, install if we don't have it
 if test ! $(which brew); then
     echo "Installing homebrew..."
@@ -42,65 +32,16 @@ brew install gnu-indent --with-default-names
 brew install gnu-which --with-default-names
 brew install gnu-grep --with-default-names
 
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
-brew install findutils
-
-# Install Bash 4
-brew install bash
-
-PACKAGES=(
-    git
-    markdown
-    npm
-    python
-    python3
-)
-
+# Install Brewfile packages
 echo "Installing packages..."
-brew install ${PACKAGES[@]}
+brew bundle --file="../homebrew/Brewfile"
 
+# Cleanup
 echo "Cleaning up..."
 brew cleanup
 
-echo "Installing cask..."
-brew install cask
-
-CASKS=(
-    r-app
-    spotify
-    spotmenu
-    visual-studio-code
-    rstudio
-    ngrok
-    station
-    virtualbox
-    iina
-    arduino
-    docker
-    cheatsheet
-    day-o
-    slack
-    balenaetcher
-    qbittorrent
-
-    ultimaker-cura
-    steam
-)
-
-echo "Installing cask apps..."
-brew cask install ${CASKS[@]}
-
-echo "Installing Python packages..."
-PYTHON_PACKAGES=(
-    virtualenv
-    virtualenvwrapper
-)
-sudo pip install ${PYTHON_PACKAGES[@]}
-
-echo "Installing global npm packages..."
-npm install marked -g
-
-echo "Configuring OSX..."
+##### macOS System Settings #####
+echo "Configuring macOS system settings..."
 
 # Require password as soon as screensaver or sleep mode starts
 defaults write com.apple.screensaver askForPassword -int 1
