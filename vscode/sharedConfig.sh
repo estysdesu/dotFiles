920ce0b869f750f23ab4442fef9069a24496f843
Tyler Estes <estysdesu@gmail.com>
2019-08-28 20:10:59 -0400

    update dotfiles from make

--- vscode/sharedConfig.sh
+++ vscode/sharedConfig.sh
@@ -4,6 +4,6 @@ stableDir="$HOME/Library/Application Support/Code/User"
 insidersDir="$HOME/Library/Application Support/Code - Insiders/User"
 
 # Link settings
-ln -sf "$stableDir/{settings.json,keybindings.json}" "$insidersDir/"
+ln -sfL "$stableDir/{settings.json,keybindings.json}" "$insidersDir/"
 ln -sfFn "$stableDir/snippets" "$insidersDir/snippets"
 
