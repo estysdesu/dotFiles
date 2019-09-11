2f93ce91ff60b5d89f5a3a267abe909a10d1a8e8
Tyler Estes <estysdesu@gmail.com>
2019-09-11 14:38:40 -0400

    update mac branch

--- sbin/addMeToSudoers.sh
+++
@@ -1,8 +0,0 @@
-newUser=`whoami`
-echo "Switching to root user"
-su
-echo "Installing sudo..."
-apt install sudo
-echo "Adding $newUser to sudoers group..."
-/usr/sbin/usermod -aG sudo $newUser
-exit 0
