edae8365f65796141e04efd4c80a8ec636e5bca1
Tyler Estes <estysdesu@gmail.com>
2019-11-11 14:55:02 -0500

    update dotfiles from make

--- _symlinks.sh
+++ _symlinks.sh
@@ -6,13 +6,9 @@ ln -sf $HOME/dotFiles/alacritty/.alacritty.yml $HOME/
 # Bash
 ln -sf $HOME/dotFiles/bash/{.bash_profile,.bashrc,.profile} $HOME/
 # Bin [user]
-<<<<<<< HEAD:symLinks.sh
-ln -sfFn $HOME/dotFiles/bin $HOME/bin
+ln -sfn $HOME/dotFiles/bin $HOME/bin
 # Homebrew
 ln -sf $HOME/dotFiles/homebrew/Brewfile $HOME/
-=======
-ln -sfn $HOME/dotFiles/bin $HOME/bin
->>>>>>> 0947b7a... update dotfiles from make:_symlinks.sh
 # Git
 ln -sf $HOME/dotFiles/git/.gitignore_global $HOME/
 # GMSH
@@ -31,13 +27,8 @@ ln -sf $HOME/dotFiles/tmux/.tmux.conf $HOME/
 ln -sf $HOME/dotFiles/vim/.vimrc $HOME/ 
 ln -sfn $HOME/dotFiles/vim/myPlugins $HOME/.vim/myPlugins 
 # VS Code
-<<<<<<< HEAD:symLinks.sh
 ln -sf $HOME/dotFiles/vscode/{settings.json,keybindings.json} "$HOME/Library/Application Support/Code/User/"
-ln -sfFn $HOME/dotFiles/vscode/snippets "$HOME/Library/Application Support/Code/User/snippets"
-=======
-# ln -sf $HOME/dotFiles/vscode/{settings.json,keybindings.json} "$HOME/Library/Application Support/Code/User/"
-# ln -sfn $HOME/dotFiles/vscode/snippets "$HOME/Library/Application Support/Code/User/snippets"
->>>>>>> 0947b7a... update dotfiles from make:_symlinks.sh
+ln -sfn $HOME/dotFiles/vscode/snippets "$HOME/Library/Application Support/Code/User/snippets"
 # sh $HOME/dotFiles/vscode/sharedConfig.sh
 # Ranger
 ln -sf $HOME/dotFiles/ranger/rc.conf $HOME/.config/ranger/
