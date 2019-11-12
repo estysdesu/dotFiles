5e36a89dc31f28c219ce34025c7a0dbe708b9d4c
Tyler Estes <estysdesu@TSE-OP990-Litt.localdomain>
2019-11-11 23:52:36 -0500

    add readme to dockerImages

--- dockerImages/symlinks.sh
+++ dockerImages/symlinks.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+imagesDir="$HOME/dotFiles/dockerImages"
 plexDir="$HOME/plexData"
 openVPNDir="$HOME/openVPNData"
+dockerFile="docker-compose.yaml"
 
-ln -sf plex.docker-compose.yaml "$plexDir"/
-ln -sf openvpn-as.docker-compose.yaml "$openVPNDir"/
+ln -sf "$imagesDir/plex.$dockerFile" "$plexDir/$dockerFile"
+ln -sf "$imagesDir/openvpn-as.$dockerFile" "$openVPNDir/$dockerFile"
