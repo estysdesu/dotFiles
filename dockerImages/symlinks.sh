88b66e984bc236f5c91294f9622947d4849acfdb
Tyler Estes <estysdesu@TSE-OP990-Litt.localdomain>
2019-11-13 13:17:22 -0500

    add symlink

--- dockerImages/symlinks.sh
+++ dockerImages/symlinks.sh
@@ -4,6 +4,8 @@ imagesDir="$HOME/dotFiles/dockerImages"
 plexDir="$HOME/plexData"
 openVPNDir="$HOME/openVPNData"
 dockerFile="docker-compose.yaml"
+ovpnDir="$HOME/ovpnData"
 
 ln -sf "$imagesDir/plex.$dockerFile" "$plexDir/$dockerFile"
 ln -sf "$imagesDir/openvpn-as.$dockerFile" "$openVPNDir/$dockerFile"
+ln -sf "$imagesDir/ovpn.$dockerFile" "$ovpnDir/$dockerFile"
