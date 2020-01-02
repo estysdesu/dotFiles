0e44bc17cd30cc4f2c7131cfeb7f53f54672bdd4
Tyler Estes <estysdesu@gmail.com>
2020-01-02 18:36:59 -0500

    update dotfiles from make

--- sbin/deb/addTmuxPlug.sh
+++ sbin/deb/addTmuxPlug.sh
@@ -2,4 +2,9 @@
 
 set -e
 
+if [ $(id -u) -eq 0 ]; then
+	echo "This cannot be run as root." >&2
+	exit 1
+fi
+
 git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
--- sbin/deb/addVimPlug.sh
+++ sbin/deb/addVimPlug.sh
@@ -2,4 +2,9 @@
 
 set -e
 
+if [ $(id -u) -eq 0 ]; then
+	echo "This cannot be run as root." >&2
+	exit 1
+fi
+
 curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
