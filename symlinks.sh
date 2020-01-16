bf1c8eb81a7d61caf150abe7325904769e3fb815
Tyler Estes <estysdesu@gmail.com>
2020-01-15 19:23:01 -0500

    reformat symlinks.sh

--- symlinks.sh
+++ symlinks.sh
@@ -1,36 +1,52 @@
 #!/usr/bin/env bash
 
 ##### set symlinks for dotFiles #####
-# Alacritty
+
+##### Alacritty #####
 ln -sf $HOME/dotFiles/alacritty/.alacritty.yml $HOME/
-# Bash
+
+##### Bash #####
 ln -sf $HOME/dotFiles/bash/{.bash_profile,.bashrc,.profile} $HOME/
-# Bin [user]
+
+##### Bin [user] #####
 ln -sfn $HOME/dotFiles/bin $HOME/bin
-# Homebrew
+
+##### Homebrew #####
 ln -sf $HOME/dotFiles/homebrew/Brewfile $HOME/
-# Git
+
+##### Git #####
 ln -sf $HOME/dotFiles/git/.gitignore_global $HOME/
-# GMSH
+
+##### GMSH #####
 ln -sf $HOME/dotFiles/gmsh/{.gmshrc,.gmsh-options} $HOME/
-# iTerm2
+
+##### iTerm2 #####
 # ln -sf $HOME/dotFiles/iterm/estysdesu.itermcolors #<path/to/iterm/conf>
-# Lepton
+
+##### Lepton #####
 ln -sf $HOME/dotFiles/lepton/.leptonrc $HOME/
-# Neofetch
+
+##### Neofetch #####
 ln -sf $HOME/dotFiles/neofetch/config.conf $HOME/.config/neofetch/
-# Node
+
+##### Node #####
 ln -sf $HOME/dotFiles/node/.npmrc $HOME/
-# Prusa Slicer
+
+##### Prusa Slicer #####
 ln -sfn $HOME/dotfiles/prusaSlicer/{printer,'print',filament} "$HOME/Library/Application Support/PrusaSlicer/"
-# Tmux
+
+##### Ranger
+ln -sf $HOME/dotFiles/ranger/rc.conf $HOME/.config/ranger/
+
+##### Tmux #####
 ln -sf $HOME/dotFiles/tmux/.tmux.conf $HOME/
-# Vim -- manual since contents are going multiple directories
+
+##### Vim -- manual since contents are going multiple directories #####
 ln -sf $HOME/dotFiles/vim/.vimrc $HOME/ 
 ln -sfn $HOME/dotFiles/vim/myPlugins $HOME/.vim/myPlugins 
-# VS Code
+
+##### VS Code #####
 ln -sf $HOME/dotFiles/vscode/{settings.json,keybindings.json} "$HOME/Library/Application Support/Code/User/"
 ln -sfn $HOME/dotFiles/vscode/snippets "$HOME/Library/Application Support/Code/User/snippets"
 # sh $HOME/dotFiles/vscode/sharedConfig.sh
-# Ranger
-ln -sf $HOME/dotFiles/ranger/rc.conf $HOME/.config/ranger/
+
