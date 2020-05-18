6a09b7cc15f9ee8aee73fad0567ce5732062824e
Tyler Estes <37026871+estysdesu@users.noreply.github.com>
2020-05-17 20:26:01 -0400

    Update addDocker.sh

--- sbin/deb/addDocker.sh
+++ sbin/deb/addDocker.sh
@@ -15,8 +15,15 @@ if [ "$DISTRO" != "debian" ] && [ "$DISTRO" != "ubuntu" ]; then
 	exit 1
 fi
 
-apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+apt install apt-transport-https \
+	ca-certificates \
+	curl \
+	gnupg-agent \
+	software-properties-common
 curl -fsSL "https://download.docker.com/linux/$DISTRO/gpg" | apt-key add -
-echo "deb [arch=amd64] https://download.docker.com/linux/$DISTRO $CODENAME stable" > /etc/apt/sources.list.d/docker.list
+echo "deb [arch=amd64] https://download.docker.com/linux/$DISTRO $CODENAME \
+	stable" > /etc/apt/sources.list.d/docker.list
 apt update
-apt install docker-ce docker-ce-cli containerd.io
+apt install docker-ce \
+	docker-ce-cli \
+	containerd.io
