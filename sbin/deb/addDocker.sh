c004a779e590713d94bff5a0df2fb76f21d56c32
Tyler Estes <37026871+estysdesu@users.noreply.github.com>
2020-05-17 20:20:34 -0400

    Update addDocker.sh

--- sbin/deb/addDocker.sh
+++ sbin/deb/addDocker.sh
@@ -2,13 +2,18 @@
 
 set -e
 
-if [ $(id -u) -ne 0 ]; then
+if [ "$(id -u)" -ne 0 ]; then
 	echo "This script needs to be run as root" >&2
 	exit 1
 fi
 
-DISTRO=$(lsb_release -is | awk '{print tolower($0)}')
-CODENAME=$(lsb_release -cs)
+DISTRO="$(lsb_release -is | awk '{print tolower($0)}')"
+CODENAME="$(lsb_release -cs)"
+
+if [ "$DISTRO" != "debian" ] && [ "$DISTRO" != "ubuntu" ]; then
+	echo "This script is only implemented on debian based distros" >&2
+	exit 1
+fi
 
 apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL "https://download.docker.com/linux/$DISTRO/gpg" | apt-key add -
