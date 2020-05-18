7b1493f9c84c7c71c07b0f03b45c3d90a6912e87
Tyler Estes <37026871+estysdesu@users.noreply.github.com>
2020-05-17 20:21:41 -0400

    Create _templateAddProgram.sh

---
+++ sbin/deb/_templateAddProgram.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+set -e
+
+if [ "$(id -u)" -ne 0 ]; then
+	echo "This script needs to be run as root" >&2
+	exit 1
+fi
+
+DISTRO="$(lsb_release -is | awk '{print tolower($0)}')"
+CODENAME="$(lsb_release -cs)"
+
+if [ "$DISTRO" != "debian" ] && [ "$DISTRO" != "ubuntu" ]; then
+	echo "This script is only implemented on debian based distros" >&2
+	exit 1
+fi
+
+##### START HERE #####
