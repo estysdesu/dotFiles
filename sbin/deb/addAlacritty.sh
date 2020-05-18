79d03b5f80ca4884d9da4630a446190b20025a15
Tyler Estes <37026871+estysdesu@users.noreply.github.com>
2020-05-17 21:11:00 -0400

    Update addAlacritty.sh

--- sbin/deb/addAlacritty.sh
+++ sbin/deb/addAlacritty.sh
@@ -8,8 +8,17 @@ if [ $(id -u) -ne 0 ]; then
 	exit 1
 fi
 
+DISTRO="$(lsb_release -is | awk '{print tolower($0)}')"
+CODENAME="$(lsb_release -cs)"
+
+if [ "$DISTRO" != "debian" ] && [ "$DISTRO" != "ubuntu" ]; then
+	echo "This script is only implemented on debian based distros" >&2
+	exit 1
+fi
+
 echo "deb http://ppa.launchpad.net/mmstick76/alacritty/ubuntu focal \
 	main" > /etc/apt/sources.list.d/alacritty.list
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 5B7FC40A404FAD98548806028AC9B4BBBAB4900B
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 \
+	--recv-keys 5B7FC40A404FAD98548806028AC9B4BBBAB4900B
 apt update
 apt install alacritty
