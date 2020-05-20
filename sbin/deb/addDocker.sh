8cf60b8097506ff4dfdfbfda46ec33ecb9384643
Tyler Estes <37026871+estysdesu@users.noreply.github.com>
2020-05-20 15:10:09 -0400

    Update addDocker.sh

--- sbin/deb/addDocker.sh
+++ sbin/deb/addDocker.sh
@@ -16,15 +16,24 @@ if [ "$DISTRO" != "debian" ] && [ "$DISTRO" != "ubuntu" ]; then
 	exit 1
 fi
 
-apt install -y apt-transport-https \
-	ca-certificates \
-	curl \
-	gnupg-agent \
-	software-properties-common
-curl -fsSL "https://download.docker.com/linux/$DISTRO/gpg" | apt-key add -
-echo "deb [arch=amd64] https://download.docker.com/linux/$DISTRO $CODENAME \
-	stable" > /etc/apt/sources.list.d/docker.list
-apt update
-apt install -y docker-ce \
-	docker-ce-cli \
-	containerd.io
+if [ "$(echo "$1" | awk '{print tolower($0)}')" == "install" ]; then
+	apt install -y apt-transport-https \
+		ca-certificates \
+		curl \
+		gnupg-agent \
+		software-properties-common
+	curl -fsSL "https://download.docker.com/linux/$DISTRO/gpg" | apt-key add -
+	echo "deb [arch=amd64] https://download.docker.com/linux/$DISTRO $CODENAME \
+		stable" > /etc/apt/sources.list.d/docker.list
+	apt update
+	apt install -y docker-ce \
+		docker-ce-cli \
+		docker-compose \
+		containerd.io
+elif [ "$(echo "$1" | awk '{print tolower($0)}')" == "uninstall" ]; then
+	echo "unimplemented..."
+	exit 1
+else
+	echo "This script can only be used to install/uninstall" >&2
+	exit 1
+fi
