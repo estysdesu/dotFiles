a99fc7350350ba96080cf3630dbdbddec8d2117e
Tyler Estes <37026871+estysdesu@users.noreply.github.com>
2020-05-20 15:31:15 -0400

    Update addDocker.sh

--- sbin/deb/addDocker.sh
+++ sbin/deb/addDocker.sh
@@ -17,22 +17,30 @@ if [ "$DISTRO" != "debian" ] && [ "$DISTRO" != "ubuntu" ]; then
 fi
 
 if [ "$(echo "$1" | awk '{print tolower($0)}')" == "install" ]; then
+	echo "installing..."
 	apt install -y apt-transport-https \
 		ca-certificates \
 		curl \
 		gnupg-agent \
 		software-properties-common
-	curl -fsSL "https://download.docker.com/linux/$DISTRO/gpg" | apt-key add -
+	curl -fsSL "https://download.docker.com/linux/$DISTRO/gpg" | apt-key --keyring /etc/apt/trusted.gpg.d/docker.gpg add - # apt key
 	echo "deb [arch=amd64] https://download.docker.com/linux/$DISTRO $CODENAME \
-		stable" > /etc/apt/sources.list.d/docker.list
+		stable" > /etc/apt/sources.list.d/docker.list # apt source
 	apt update
 	apt install -y docker-ce \
 		docker-ce-cli \
 		docker-compose \
-		containerd.io
+		containerd.io # apt packages
 elif [ "$(echo "$1" | awk '{print tolower($0)}')" == "uninstall" ]; then
-	echo "unimplemented..."
-	exit 1
+	echo "uninstalling..."
+	rm /etc/apt/trusted.gpg.d/docker.gpg # apt key
+	rm /etc/apt/sources.list.d/docker.list # apt source
+	apt remove --purge -y docker-ce \
+		docker-ce-cli \
+		docker-compose \
+		containerd.io \
+		&& apt autoremove # apt packages
+	apt update
 else
 	echo "This script can only be used to install/uninstall" >&2
 	exit 1
